a5f54e126bf70a81fa8657d4986ec1f73ed22cd4
Gobezar <vadimkiselevv@yandex.ru>
2023-07-06 01:46:22 +0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="root">
         <div className="root-layout">
           <ReactQueryProvider>
             <ThemeProviders>
