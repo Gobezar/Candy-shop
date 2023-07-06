96323b1a0f592360a7425859f02572975fbcade1
Gobezar <vadimkiselevv@yandex.ru>
2023-07-06 16:50:25 +0400

    fix metadata

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,50 @@
-// import ProductPage from "../../../pages/product/UI/product";
+// // import ProductPage from "../../../pages/product/UI/product";
+// // import { Metadata } from "next";
+
+// // export const metadata: Metadata = {
+// //   title: "Карточка товара | Candy Shop",
+// // };
+// // export default ProductPage;
+
+// import React from "react";
+// import Navbar from "@/widgets/Navbar/UI/Navbar";
+// import CategoriesList from "@/widgets/CategoriesList/UI/CategoriesList";
+// import ProductCharacteristics from "@/entities/ProductCharacteristics/UI/ProductCharacteristics";
+// import SwiperSlider from "@/widgets/SwiperSlider/UI/SwiperSlider";
+// import CategoryFeed from "@/widgets/CategoryFeed/UI/CategoryFeed";
 // import { Metadata } from "next";
 
 // export const metadata: Metadata = {
 //   title: "Карточка товара | Candy Shop",
 // };
+
+// type ProductPageProps = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// export function generateMetadata({ params: { id } }: ProductPageProps) {
+//   return {
+//     title: "Продукт " + id,
+//   };
+// }
+
+// const ProductPage: React.FC<ProductPageProps> = ({ params: { id } }) => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <CategoriesList />
+//       <ProductCharacteristics id={id} />
+//       <SwiperSlider couples={true}>С этим товаром покупают</SwiperSlider>
+//       <CategoryFeed />
+//       <SwiperSlider id={id} similar={true}>
+//         Похожие товары
+//       </SwiperSlider>
+//     </div>
+//   );
+// };
+
 // export default ProductPage;
 
 import React from "react";
@@ -14,17 +55,15 @@ import SwiperSlider from "@/widgets/SwiperSlider/UI/SwiperSlider";
 import CategoryFeed from "@/widgets/CategoryFeed/UI/CategoryFeed";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Карточка товара | Candy Shop",
-};
-
 type ProductPageProps = {
   params: {
     id: string;
   };
 };
 
-export function generateMetadata({ params: { id } }: ProductPageProps) {
+export function generateMetadata({
+  params: { id },
+}: ProductPageProps): Metadata {
   return {
     title: "Продукт " + id,
   };
