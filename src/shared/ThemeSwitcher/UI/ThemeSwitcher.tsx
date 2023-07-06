15c93302d93875db3fa84b638b8ae362b2f9774b
Gobezar <vadimkiselevv@yandex.ru>
2023-07-06 19:05:13 +0400

    fix toggleTheme

--- src/shared/ThemeSwitcher/UI/ThemeSwitcher.tsx
+++ src/shared/ThemeSwitcher/UI/ThemeSwitcher.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Switch } from "antd";
 import cl from "./ThemeSwitcher.module.scss";
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme");
+    currentTheme
+      ? setTheme(currentTheme)
+      : localStorage.setItem("theme", "light");
+  }, []);
 
   const changeTheme = () => {
     if (theme === "light") {
