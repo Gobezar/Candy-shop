b27030fe29d90161463fbd261046b41bead337cf
Gobezar <vadimkiselevv@yandex.ru>
2023-07-06 16:16:21 +0400

    fix

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,40 @@
-import Catalog from "@/pages/catalog/UI/catalog";
+// import Catalog from "@/pages/catalog/UI/catalog";
+// import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "Каталог | Candy Shop",
+// };
+// export default Catalog;
+
+import Navbar from "@/widgets/Navbar/UI/Navbar";
+import Search from "@/features/Search/UI/Search";
+import Sort from "@/features/Sort/UI/Sort";
+import CategoriesList from "@/widgets/CategoriesList/UI/CategoriesList";
+import ProductsList from "@/widgets/ProductsList/UI/ProductsList";
+import cl from "./catalog.module.scss";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Каталог | Candy Shop",
 };
+
+const Catalog = () => {
+  return (
+    <div>
+      <Navbar>
+        <Search />
+      </Navbar>
+      <CategoriesList />
+      <div className={cl.contentPage}>
+        <div className={cl.sortBlock}>
+          <Sort />
+        </div>
+        <div className={cl.productListBlock}>
+          <ProductsList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Catalog;
